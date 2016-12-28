50747265ce88911940f990370dccce12adf036e5
DESKTOP-6GDQRNU\yabba <yabba1961@gmail.com>
2016-12-28 19:36:59 +0100

    Textual adjustments

--- 101-vm-simple-windows/CallingScript.ps1
+++ 101-vm-simple-windows/CallingScript.ps1
@@ -23,12 +23,32 @@ Login-AzureRmAccount
 # Adjust the 'yournamehere' part of these three strings to
 # something unique for you. Leave the last two characters in each.
 $URI       = 'https://raw.githubusercontent.com/yabba1961/ARM-Templates/master/101-vm-simple-windows/azuredeploy.json'
-$T_URI     = 'https://raw.githubusercontent.com/yabba1961/ARM-Templates/master/101-vm-simple-windows/azuredeploy.parameters.json'
+# $T_URI     = 'https://raw.githubusercontent.com/yabba1961/ARM-Templates/master/101-vm-simple-windows/azuredeploy.parameters.json'
 $Location  = 'west europe'
 $rgname    = 'YabbasRG'
 $saname    = 'yabbassa'     # Lowercase required
+$addnsName = 'yabbasad'     # Lowercase required
 
 # Create the new resource group. Runs quickly.
 New-AzureRmResourceGroup -Name $rgname -Location $Location
+
+# Parameters for the template and configuration
+$MyParams = @{
+#    newStorageAccountName = $saname
+#    location              = 'West Europe'
+    dnsLabelPrefix        = $addnsName
+   }
+
+# Splat the parameters on New-AzureRmResourceGroupDeployment  
+$SplatParams = @{
+    TemplateUri             = $URI 
+    ResourceGroupName       = $rgname 
+    TemplateParameterObject = $MyParams
+    Name                    = 'BedrockForest'
+   }
+
+# This takes ~10 minutes
+# One prompt for the domain admin password
+New-AzureRmResourceGroupDeployment @SplatParams -Verbose
 
 New-AzureRmResourceGroupDeployment -Name Simple -ResourceGroupName $rgname -TemplateFile $URI -TemplateParameterFile $T_URI
